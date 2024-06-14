3198a9c6981e5c1bdd50a16ca54243bf2b890513
Ownosh <144677630+Ownosh@users.noreply.github.com>
2024-06-15 02:51:43 +0300

    Делаем окно по центру и добавляем изображение

--- memory.py
+++ memory.py
@@ -1,17 +1,25 @@
-from tkinter import Tk, Canvas, Button
+from tkinter import Tk, Canvas, Button, PhotoImage
 
 class MainMenu:
     def __init__(self):
         self.root = Tk()
         self.root.title("Запомни одинаковые")
-        self.root.geometry("900x500")
+        self.center_window(900, 500)
 
         self.canvas = Canvas(self.root, width=900, height=500, highlightthickness=0)
         self.canvas.pack()
 
         self.create_buttons()
+        self.add_images()
 
         self.root.mainloop()
+
+    def center_window(self, width, height):
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        x = (screen_width / 2) - (width / 2)
+        y = (screen_height / 2) - (height / 2)
+        self.root.geometry(f'{width}x{height}+{int(x)}+{int(y)}')
 
     def create_buttons(self):
         play_button = Button(self.root, text="Играть", fg="Black", font=('times new roman', 15), bg='#F4FBED',
@@ -25,6 +33,10 @@ class MainMenu:
         exit_button = Button(self.root, text="Выход", fg="Black", font=('times new roman', 15), bg='#F4FBED',
                              width=18, height=1, command=self.exit_game)
         self.canvas.create_window(560, 320, anchor='nw', window=exit_button)
+
+    def add_images(self):
+        self.img_obj3 = PhotoImage(file="img_rules_menu/dio.png")
+        self.canvas.create_image(0, 0, anchor='nw', image=self.img_obj3)
 
     def start_game(self):
         print("Начать игру")
